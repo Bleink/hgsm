33973636e14f05f2293d1254d91f0d92411e2bd9
gfasil <fhabtegiorgis@mum.edu>
2020-06-16 13:21:29 -0500

    stash

--- hotelguestmanagementsystem/src/main/java/edu/miu/cs/cs425/seniorproject/hotelguestmanagementsystem/controller/ReservationController.java
+++ hotelguestmanagementsystem/src/main/java/edu/miu/cs/cs425/seniorproject/hotelguestmanagementsystem/controller/ReservationController.java
@@ -4,6 +4,8 @@ import edu.miu.cs.cs425.seniorproject.hotelguestmanagementsystem.dto.Reservation
 import edu.miu.cs.cs425.seniorproject.hotelguestmanagementsystem.model.Reservation;
 import edu.miu.cs.cs425.seniorproject.hotelguestmanagementsystem.model.Room;
 import edu.miu.cs.cs425.seniorproject.hotelguestmanagementsystem.service.ReservationService;
+import edu.miu.cs.cs425.seniorproject.hotelguestmanagementsystem.service.RoomService;
+import edu.miu.cs.cs425.seniorproject.hotelguestmanagementsystem.service.RoomTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +13,29 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletContext;
 import java.security.Principal;
 
 @Controller
 public class ReservationController {
     @Autowired
     private ReservationService reservationService;
+
+    @Autowired
+    private RoomService roomService;
+
+    @Autowired
+    private RoomTypeService roomTypeService;
+    @Autowired
+    ServletContext servletContext;
+    @GetMapping("/reservation")
+    public ModelAndView checkReservation() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("roomTypes", roomTypeService.getAllRoomTypes());
+        modelAndView.addObject("reservationDto", new ReservationDto());
+
+        modelAndView.setViewName("reservation");
+        return modelAndView;}
 
         @ModelAttribute("reservationDto")
         public ReservationDto reservationDto(){
@@ -34,10 +53,6 @@ public class ReservationController {
      return modelAndView;
     }
 
-    @GetMapping("/reservation")
-    public String checkReservation() {
 
-        return "index";
-    }
 
 }
